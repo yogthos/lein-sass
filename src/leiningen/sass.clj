736eafdc4352d28966f8c84ec7fae9f4b007ecc3
Yogthos <dmitri.sotnikov@gmail.com>
2015-04-12 14:43:18 -0400

    added file watching

--- src/leiningen/sass.clj
+++ src/leiningen/sass.clj
@@ -1,8 +1,10 @@
 (ns leiningen.sass
-  (:require [clojure.java.io :as io])
+  (:require [clojure.set :refer [rename-keys]]
+            [clojure.java.io :as io])
   (:import
-   java.io.File
-   [javax.script Invocable ScriptEngine ScriptEngineManager]))
+    java.io.File
+    [java.nio.file FileSystems Paths StandardWatchEventKinds]
+    [javax.script Invocable ScriptEngineManager]))
 
 (def compiled? (atom false))
 (def engine (.getEngineByName (ScriptEngineManager.) "nashorn"))
@@ -10,6 +12,35 @@
 (.eval engine "var source = '';")
 (.eval engine "function setSource(input) {source = input;};")
 
+(defn register-events! [dir watch-service]
+  (.register dir
+             watch-service
+             (into-array
+               [StandardWatchEventKinds/ENTRY_CREATE
+                StandardWatchEventKinds/ENTRY_MODIFY
+                StandardWatchEventKinds/ENTRY_DELETE
+                StandardWatchEventKinds/OVERFLOW])
+             (into-array [(com.sun.nio.file.SensitivityWatchEventModifier/HIGH)])))
+
+(defn watch-loop [watch-service handler]
+  (while true
+    (when-let [k (.take watch-service)]
+      (doseq [event (.pollEvents k)]
+        (handler event))
+      (.reset k))))
+
+(defn watch [path handler]
+  (let [dir (-> path (io/file) (.toURI) (Paths/get))]
+    (with-open [watch-service (.newWatchService (FileSystems/getDefault))]
+      (register-events! dir watch-service)
+      (watch-loop watch-service handler))))
+
+(defn watch-thread [path handler]
+  (println "watching for changes in" path)
+  (doto
+    (Thread. #(watch path handler))
+    (.start)
+    (.join)))
 
 (defn compile-file [file]
   (let [source (slurp file)]
@@ -27,19 +58,26 @@
 (defn ext-sass->css [file-name]
   (str (subs file-name 0 (.lastIndexOf file-name ".")) ".css"))
 
-(defn sass [{{:keys [source target]} :sass} & opts]
-  (when (not @compiled?)
-    (let [files (find-assets (io/file source) ".sass")]
-      (doseq [file files]
+(defn compile-assets [files target]
+  (doseq [file files]
+    (let [compiled  (compile-file file)
+          file-name (.getName file)]
+      (println "compling" file-name)
+      (if (string? compiled)
+        (do
+          (println "compiled successfully")
+          (spit (str target File/separator (ext-sass->css file-name)) compiled))
+        (do
+          (println "failed to compile:" file-name)
+          (clojure.pprint/pprint compiled))))))
 
-        (let [compiled (compile-file file)
-              file-name (.getName file)]
-          (println "compling" file-name)
-          (if (map? compiled)
-            (do
-              (println "failed to compile:" file-name)
-              (clojure.pprint/pprint compiled))
-            (do
-              (println "compiled successfully")
-              (spit (str target File/separator (ext-sass->css file-name)) compiled)))))
-      (reset! compiled? true))))
+(defn sass [{{:keys [source target]} :sass} & opts]
+  (let [files (concat (find-assets (io/file source) ".sass")
+                      (find-assets (io/file source) ".scss"))]
+    (if (some #{"watch"} opts)
+      (do
+        (compile-assets files target)
+        (watch-thread source (fn [_] (compile-assets files target))))
+      (when (not @compiled?)
+        (compile-assets files target)
+        (reset! compiled? true)))))
