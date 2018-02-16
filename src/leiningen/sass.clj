f460c291e94f57b89fc239c40b8103ded02e258d
Edward Knyshov <edvorg@gmail.com>
2018-02-16 20:46:31 +0700

    fix source map url

--- src/leiningen/sass.clj
+++ src/leiningen/sass.clj
@@ -133,7 +133,8 @@
           (let [map-file-path (str output-file-path ".map")]
             (io/make-parents output-file-path)
             (spit output-file-path text)
-            (spit output-file-path (format "\n/*# sourceMappingURL=%s.map */\n" file-name) :append true)
+            (spit output-file-path (format "\n/*# sourceMappingURL=%s.map */\n" output-file-name)
+                  :append true)
             (spit map-file-path map)))))))
 
 (defn sass [{{:keys [source target]} :sass} & opts]
